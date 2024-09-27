02aedae14f247c30a2ad5f74dc92b79668276e6a
jenisha <angelinejenisha05@gmail.com>
2024-09-27 22:25:24 +0530

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,117 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+   numb: 6, 
+   question:"React.js is written in which of the following language?",
+   answer:"JavaScript",
+   options:[
+      "C++",
+      "C",
+      "Java",
+      "JavaScript"
+ ]
+  },
+
+  {
+  numb:7,
+  question:"Which among these is not a Web browser?",
+  answer:"www",
+  options:[
+    "www",
+    "Chrome",
+    "Opera",
+    "NetSurf"
+  ]
+  },
+
+  {
+    numb:8,
+    question:"The correct sequence of HTML tags for starting a webpage is", 
+    answer:"HTML, Head, Title, Body",
+    options:[
+    "Head, Title, HTML, body",
+    "HTML, Body, Title, Head",
+    "HTML, Head, Title, Body",
+    "HTML, Head, Title, Body"
+    ]
+  },
+  {
+    numb:9,
+    question:"The web is based on",
+    answer:"HTML",
+    options:[
+    "Images",
+   "Text",
+    "Information",
+    "HTML"
+    ]
+  },
+
+  {
+    numb:10,
+    question:"What is Git?",
+    answer:"Version control system",
+    options:[
+      "Framework",
+      "Version control system",
+      "Database",
+      "Package manager"
+    ]
+
+  },
+  
+];
